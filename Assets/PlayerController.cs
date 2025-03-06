6cef44981701f5538d8338d5178c146a1db66b2b
oneprize <bigsky010101@gmail.com>
2025-03-07 00:54:37 +0900

    Dodge 20250307
    7장까지

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,36 +1,30 @@
-using JetBrains.Annotations;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody playerRigidbody;
-    public float speed = 8f;
-    
+
+   public Rigidbody playerRigidbody;
+   public float speed = 8f;
+
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
-    
+   
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow)==true)
-        {
-            playerRigidbody.AddForce(0f, 0f, speed);
-        }
-        if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, -speed);
-        }
-        if (Input.GetKey(KeyCode.RightArrow) == true)
-        {
-            playerRigidbody.AddForce(speed, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-            playerRigidbody.AddForce(-speed, 0f, 0f);
-        }
-        
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+
+        playerRigidbody.linearVelocity = newVelocity;
+
+
     }
     public void Die()
     {
